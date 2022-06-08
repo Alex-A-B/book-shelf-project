1dcd7ace0dcd1a20d92d0fe9e584ca458248795d
Alex-A-B <alexjamesbelton@gmail.com>
2022-06-08 10:08:50 +0100

    linking models in API complete - testing the aliases for the has_many links.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,6 @@
 class Book < ApplicationRecord
+    
+    has_many :user_comments, through: :comments, source: :users
+    has_many :user_bookshelves, through: :bookshelves, source: :users
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+
+    has_secure_password
+
+    validates :username, uniqueness: true
+    
+    #alias has_many connections
+    has_many :commented_books, through: :comments, source: :books
+    has_many :shelved_books, through: :bookshelves, source: :books
+
+
 end
